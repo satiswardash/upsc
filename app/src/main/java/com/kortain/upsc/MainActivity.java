1c0c0dc2b515a092b044621d70d4e7e750f720e0
Satiswar Dash <Satiswar.Dash@incture.com>
2017-12-19 15:43:29 +0530

    Authentication activity (signin & register) layouts were created.

--- app/src/main/java/com/kortain/upsc/MainActivity.java
+++ app/src/main/java/com/kortain/upsc/MainActivity.java
@@ -1,13 +1,22 @@
 package com.kortain.upsc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean auth = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (!auth) {
+            Intent intent = new Intent(MainActivity.this, AuthenticationActivity.class);
+            startActivity(intent);
+        }
+
         setContentView(R.layout.activity_main);
     }
 }
